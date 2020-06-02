fed70576282073347a9074e758e2c1bbc510df88
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-02 21:30:13 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+        id INTEGER PRIMARY KEY,
+                it INTEGER,
+                name TEXT,
+                age INTEGER,
+                gender TEXT,
+                color TEXT,
+                temperament TEXT,
+                alive BOOLEAN
+              )
+end
